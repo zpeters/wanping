2299e94602c11255259d080421328a8eb139143b
Zach Peters <zpeters@gmail.com>
2019-05-21 09:55:06 -0500

    Added additional tests

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,26 @@ fn main() {
     }
 
     let addrs = &args[1..];
-    let mut results = Vec::new();
+    let mut pings = Vec::new();
 
     for e in addrs {
-        results.push(pinger::ping(e))
+        pings.push(pinger::ping(e))
     }
 
-    let all_up = results.iter().all(|&x| x);
-    let all_down = results.iter().all(|&x| !x);
+    println!("{}", results(pings))
+   
+}
+
+fn results(pings: Vec<bool>) -> String {
+    let all_up = pings.iter().all(|&x| x);
+    let all_down = pings.iter().all(|&x| !x);
 
     if all_up {
-        println!("ALLUP: All ips are up")
+        "ALLUP: All ips are up".to_string()
     } else if all_down {
-        println!("ALLDOWN: All ips are down")
+        "ALLDOWN: All ips are down".to_string()
     } else {
-        println!("SOMEDOWN: Some ips are down")
+        "SOMEDOWN: Some ips are down".to_string()
     }
 }
 
@@ -37,4 +42,76 @@ fn help() {
     println!("\twanping 1.1.1.1");
     println!("\twanping reddit.com");
     println!("\twanping 1.1.1.1 8.8.8.8 reddit.com");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_all_up_single() {
+        let mut pings = Vec::new();
+        pings.push(true);
+        assert_eq!(results(pings), "ALLUP: All ips are up");
+    }
+
+    #[test]
+    fn test_all_up_multiple() {
+        let mut pings = Vec::new();
+        pings.push(true);
+        pings.push(true);
+        pings.push(true);
+        pings.push(true);
+        assert_eq!(results(pings), "ALLUP: All ips are up");
+    }
+  
+    #[test]
+    fn test_some_down() {
+        let mut pings = Vec::new();
+        pings.push(true);
+        pings.push(false);
+        assert_eq!(results(pings), "SOMEDOWN: Some ips are down");
+    }
+
+     #[test]
+    fn test_some_down_multiple() {
+        let mut pings = Vec::new();
+        pings.push(true);
+        pings.push(false);
+        pings.push(false);
+        pings.push(true);
+        pings.push(false);
+        pings.push(true);
+        pings.push(true);
+        pings.push(false);
+        pings.push(false);
+        pings.push(true);
+        pings.push(false);
+        pings.push(true);
+        pings.push(true);
+        pings.push(false);
+        pings.push(false);
+        pings.push(true);
+        pings.push(false);
+        pings.push(true);
+        assert_eq!(results(pings), "SOMEDOWN: Some ips are down");
+    }
+    #[test]
+    fn test_all_down_single() {
+        let mut pings = Vec::new();
+        pings.push(false);
+        assert_eq!(results(pings), "ALLDOWN: All ips are down");
+    }
+
+    #[test]
+    fn test_all_down_multiple() {
+        let mut pings = Vec::new();
+        pings.push(false);
+        pings.push(false);
+        pings.push(false);
+        pings.push(false);
+        assert_eq!(results(pings), "ALLDOWN: All ips are down");
+    }
+  
+
 }
