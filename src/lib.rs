43a33f8f7685e6499eee2437ed1ef21cd909e959
Zach Peters <zpeters@bcianswers.com>
2020-07-22 11:44:07 -0500

    Additional cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,8 @@
-extern crate fern;
-#[macro_use]
-extern crate log;
-extern crate chrono;
-
 pub mod pinger {
     use std::process::Command;
     use std::str;
 
-    fn setup_logger() -> Result<(), fern::InitError> {
-        fern::Dispatch::new()
-            .format(|out, message, record| {
-                out.finish(format_args!(
-                    "{}[{}][{}] {}",
-                    chrono::Local::now().format("[%Y-%m-%dT%H:%M:%S-06:00]"),
-                    record.target(),
-                    record.level(),
-                    message
-                ))
-            })
-            .level(log::LevelFilter::Warn)
-            .chain(std::io::stdout())
-            .apply()?;
-        Ok(())
-    }
-
     pub fn ping(ip: &str, timeout: &str) -> bool {
-        let _ = setup_logger();
-
         let output = if cfg!(windows) {
             Command::new("cmd")
                 .args(&["/C", "ping -n 1 -w", timeout, ip])
@@ -43,38 +19,19 @@ pub mod pinger {
 
         if cfg!(windows) {
             match str::from_utf8(&output) {
-                Ok(result) => {
-                    if result.contains("(0% loss)") {
-                        debug!("Ping {} success", ip);
-                        true
-                    } else {
-                        warn!("Ping {} failed", ip);
-                        false
-                    }
-                }
+                Ok(result) => result.contains("(0% loss)"),
                 Err(e) => {
-                    warn!("Ping {} failed", ip);
-                    error!("Error: {}", e);
+                    println!("Error: {:?}", e);
                     false
                 }
             }
         } else {
             match str::from_utf8(&output) {
                 Ok(result) => {
-                    if result.contains(" 0.0% packet loss") {
-                        debug!("Ping {} success", ip);
-                        true
-                    } else if result.contains(" 0% packet loss") {
-                        debug!("Ping {} success", ip);
-                        true
-                    } else {
-                        warn!("Ping {} failed", ip);
-                        false
-                    }
+                    result.contains(" 0.0% packet loss") || result.contains(" 0% packet loss")
                 }
                 Err(e) => {
-                    warn!("Ping {} failed", ip);
-                    error!("Error: {}", e);
+                    println!("Error: {:?}", e);
                     false
                 }
             }
